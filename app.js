73e9068dc18b8490481bf31c97df29e6e10ce8b3
Martin Yanchev <martinm199913@gmail.com>
2021-01-20 04:37:24 +0000

    Fixing some style problems

--- app.js
+++ app.js
@@ -14,14 +14,18 @@ function handleCalculate(e){
     //preventing default handling
     e.preventDefault();
     // checking if the input is valid
-    if(numberField.value>0 && numberField.value<=250){ 
+    if(numberField.value>=0 && numberField.value<=250){ 
         // If true creating anime object with wanted properties
+        result.style.color = 'black';
+
         anime({
             targets: result,
             innerHTML: [0,fib(numberField.value)], // execute fib with the given value and populate result with the answer animate from zero to the desired value
             easing: 'linear',
-            round: 10
+            round: 10,
+            color: 'black'
         });
+        numberField.value = 0;
 
     }else{
         //if input is not valid we print the following error message
@@ -32,7 +36,7 @@ function handleCalculate(e){
 
  function fib(number){
      if(number <=1){
-         return 1;
+         return number;
      }
      if(memory[number]){
          return memory[number];
